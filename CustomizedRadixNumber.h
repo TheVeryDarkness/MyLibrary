8d1f4a33ac0435794c8581ffe2cd2052763c6bf0
博奕 黄 <1853308@tongji.edu.cn>
2019-07-22 22:10:04 +0800

    Update CustomizedRadixNumber.h

--- CustomizedRadixNumber.h
+++ CustomizedRadixNumber.h
@@ -1,19 +1,19 @@
 #pragma once
+#include "Shared.h"
 
 namespace LargeInteger {
 	template<typename Data, Data Radix>
 	class Num
 	{
 	public:
-		Num()
-		{
-		}
-
-		~Num()
-		{
+		Num(Data init) :data(data) {}
+		~Num(){}
+		Data MY_LIBRARY ComplementCode()const { return (Radix - data); }
+		void MY_LIBRARY ADD(const Num& that){
+			throw;
 		}
 
 	private:
-
+		Data data;
 	};
 }
