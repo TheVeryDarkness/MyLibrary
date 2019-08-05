71c330ef465f5c8c1d6c38a494fd2ee5f9f90d23
博奕 黄 <1853308@tongji.edu.cn>
2019-08-05 12:22:30 +0800

    Update CustomizedBaseTraits.h

--- CustomizedBaseTraits.h
+++ CustomizedBaseTraits.h
@@ -1,19 +1,40 @@
 #pragma once
 
 #include <istream>
-#include <stack>
+#include <iterator>
 
 namespace LargeInteger {
 
-	//Mind that '?' is reserved for unknown input
-	template<typename _Elem, size_t BeginIndex, _Elem Head, _Elem... Remained>
-	class OddTraits :OddTraits<_Elem, BeginIndex + 1, Remained>
+	template<typename char_type, typename int_type, size_t BeginIndex, char_type... set>
+	class BaseSet;
+
+	template<typename char_type, typename int_type, size_t BeginIndex>
+	class BaseSet<char_type, int_type, BeginIndex>
 	{
 	public:
-		MY_LIBRARY OddTraits() = default;
-		MY_LIBRARY ~OddTraits() = default;
-		using char_type=typename OddTraits<_Elem, BeginIndex + 1, Remained>::char_type;
-		using int_type=typename OddTraits<_Elem, BeginIndex + 1, Remained>::int_type;
+		MY_LIBRARY BaseSet() = delete;
+		MY_LIBRARY ~BaseSet() = delete;
+
+		static int_type MY_LIBRARY to_int_type(char_type Char)noexcept {
+			return int_type('?');
+		}
+		static char_type MY_LIBRARY to_char_type(int_type Int)noexcept {
+			return char_type('?');
+		}
+		static size_t MY_LIBRARY getRadix()noexcept {
+			return BeginIndex;
+		}
+	private:
+
+	};
+
+	//Mind that '?' is reserved for unknown input
+	template<typename char_type, typename int_type, size_t BeginIndex, char_type Head, char_type... Remained>
+	class BaseSet<char_type, int_type, BeginIndex, Head, Remained...> :public BaseSet<char_type, int_type, BeginIndex + 1, Remained...>
+	{
+	public:
+		MY_LIBRARY BaseSet() = delete;
+		MY_LIBRARY ~BaseSet() = delete;
 		static int_type MY_LIBRARY to_int_type(char_type Char)noexcept {
 			if constexpr (Char == Head)
 			{
@@ -21,7 +42,7 @@ namespace LargeInteger {
 			}
 			else
 			{
-				return OddTraits<_Elem, BeginIndex + 1, Remained>::to_int_type(Char);
+				return BaseSet<char_type, int_type, BeginIndex + 1, Remained>::to_int_type(Char);
 			}
 		}
 		static char_type MY_LIBRARY to_char_type(int_type Int)noexcept {
@@ -31,65 +52,43 @@ namespace LargeInteger {
 			}
 			else
 			{
-				return OddTraits<_Elem, BeginIndex + 1, Remained>::to_char_type(Int);
+				return BaseSet<char_type, int_type, BeginIndex + 1, Remained>::to_char_type(Int);
 			}
+		}
+		static size_t MY_LIBRARY getRadix()noexcept {
+			return BaseSet<char_type, int_type, BeginIndex + 1, Remained...>::getRadix();
 		}
 	private:
 
 	};
 
+	typedef BaseSet < char, unsigned char, 0, '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'> Set16;
+	typedef BaseSet < char, unsigned char, 0, '0', '1', '2', '3', '4', '5', '6', '7', '8'> Set8;
+	typedef BaseSet < char, unsigned char, 0, '0', '1'> Set2;
+	typedef BaseSet < char, unsigned char, 0, '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'> Set10;
 
-	template<typename _Elem,size_t BeginIndex, _Elem Head>
-	class OddTraits<_Elem, BeginIndex, Head> :std::char_traits<_Elem>
-	{
-	public:
-		using char_type=typename std::char_traits<_Elem>::char_type;
-		using int_type=typename std::char_traits<_Elem>::int_type;
-		MY_LIBRARY OddTraits() = default;
-		MY_LIBRARY ~OddTraits() = default;
-
-		static int_type MY_LIBRARY to_int_type(_Elem Char)noexcept {
-			if constexpr (Char == Head)
-			{
-				return BeginIndex;
-			}
-			else
-			{
-				return int_type('?');
-			}
-		}
-		static char_type MY_LIBRARY to_char_type(int_type Int)noexcept {
-			if constexpr (Int == BeginIndex)
-			{
-				return char_type(Head);
-			}
-			else
-			{
-				return char_type('?');
-			}
-		}
-	private:
-
-	};
-	
-	template<typename _Elem,typename _Traits>
+	template<typename _Elem, typename charset>
 	class OddStream
 	{
 	public:
-		OddStream(std::basic_istream<_Elem> i)noexcept:i(i){}
+		OddStream(std::basic_istream<_Elem> i)noexcept :i(i) {}
 
 		~OddStream() = default;
 
 
-		OddStream& MY_LIBRARY operator>>(std::basic_string<_Elem,_Traits> str)noexcept {
-			std::basic_string<_Elem, _Traits> temp;
+		template<typename Iter>
+		OddStream& MY_LIBRARY operator>>(Iter str)noexcept {
+			std::basic_string<_Elem> temp;
 			is >> temp;
+			for (auto& i : temp)
+			{
+				str = charset::to_int_type(i);
+				++str;
+			}
 			return *this;
 		}
-
 
 	private:
 		std::basic_istream<_Elem> is;
 	};
-
-}
+};
