4406169d0e7a23ff44e30be69afc2cb1a6b7e6d4
博奕 黄 <1853308@tongji.edu.cn>
2019-08-15 14:05:02 +0800

    Update _Bytes.h

--- _Bytes.h
+++ _Bytes.h
@@ -72,7 +72,18 @@ namespace Integer {
 		constexpr void _SHR(bool fill, bool& tail)noexcept {
 			return;
 		}
+		constexpr void Odd(bool& buf)noexcept {}
+		constexpr bool NonZero()const noexcept {
+			return true;
+		}
+		constexpr bool PlusOne()noexcept {
+			return true;
+		}
+		constexpr bool MinusOne()noexcept {
+			return true;
+		}
 	public:
+		constexpr size_t MY_LIBRARY GetBits()const noexcept { return 0; }
 		constexpr MY_LIBRARY _Bytes() = default;
 		MY_LIBRARY ~_Bytes() = default;
 		constexpr bool operator>(const _Bytes& that)const noexcept {
@@ -177,15 +188,58 @@ namespace Integer {
 			}
 			this->_Bytes<IntelligentLength(Length).first>::SHR(temp);
 		}
+		constexpr void _SHR(bool fill, bool& tail)noexcept {
+			bool temp = this->Byte & 0x1;
+			this->Byte >>= 1;
+			if (fill)
+			{
+				this->Byte |= value_type(value_type(1) << (sizeof(value_type) * LargeInteger::BitsPerByte - 1));
+			}
+			tail = this->Byte & 0x1;
+			return this->_Bytes<IntelligentLength(Length).first>::_SHR(temp, tail);
+		}
+		constexpr bool Odd()noexcept {
+			if constexpr (IntelligentLength(Length).first == 0)
+				return (this->Byte & 0x1);
+			else return this->_Bytes<IntelligentLength(Length).first>::Odd();
+		}
+		constexpr bool PlusOne()noexcept {
+			if (this->_Bytes<IntelligentLength(Length).first>::PlusOne())
+			{
+				bool temp = (this->Byte == std::numeric_limits<value_type>::max());
+				this->Byte += 1;
+				return temp;
+			}
+			else return false;
+		}
+		constexpr bool MinusOne()noexcept {
+			if (this->_Bytes<IntelligentLength(Length).first>::MinusOne())
+			{
+				bool temp = (this->Byte == 0);
+				this->Byte -= 1;
+				return temp;
+			}
+			else return false;
+		}
+		_Bytes(const _Bytes& that) = default;
 	public:
 		using value_type=decltype(Byte);
 		template<typename Val, typename... Vals>
 		constexpr MY_LIBRARY _Bytes(Val val, Vals... vals)
 			noexcept : Byte(val), _Bytes<IntelligentLength(Length).first>(vals...) {}; //= default;
+		constexpr MY_LIBRARY _Bytes()
+			noexcept :Byte(0), _Bytes<IntelligentLength(Length).first>() {};
 		MY_LIBRARY ~_Bytes() = default;
 		constexpr _Bytes& Comple()noexcept {
 			this->Comp();
 			return *this;
+		}
+		constexpr bool NonZero()const noexcept {
+			if (this->Byte != 0)
+			{
+				return false;
+			}
+			else return this->_Bytes<IntelligentLength(Length).first>::NonZero();
 		}
 		constexpr _Bytes& operator<<=(size_t sz)noexcept {
 			for (size_t i = 0; i < sz; i++) {
@@ -206,6 +260,67 @@ namespace Integer {
 		constexpr _Bytes& operator-=(const _Bytes& that)noexcept {
 			this->sub_u(that);
 			return *this;
+		}
+		constexpr _Bytes& operator++()noexcept {
+			return *this;
+		}
+		constexpr _Bytes& operator--()noexcept {
+			return *this;
+		}
+		constexpr _Bytes& MY_LIBRARY operator*=(const _Bytes& that)noexcept {
+			_Bytes This(*this);
+			memset(this, 0, Length);
+			auto temp = that;
+			bool Will;
+			for (size_t j = 0; j < Length * LargeInteger::BitsPerByte && temp.NonZero(); ++j)
+			{
+				Will = temp.Odd();
+				if (Will) {
+					*this += This;
+				}
+				This.SHL();
+				//temp._SHR(false, Will);
+				temp.SHR(false);
+			}
+			return *this;
+		}
+		constexpr size_t MY_LIBRARY GetBits()const noexcept {
+			if (this->Byte == 0) {
+				return this->_Bytes<IntelligentLength(Length).first>::GetBits();
+			}
+			else {
+				return (IntelligentLength(Length).first * LargeInteger::BitsPerByte + getBits(this->Byte));
+			}
+		}
+		constexpr _Bytes MY_LIBRARY Divide(const _Bytes& that)noexcept {
+			if (*this < that) {
+				assert(this->GetBits() <= that.GetBits());
+				return _Bytes();
+			}
+			else if (*this == that) {
+				assert(this->GetBits() == that.GetBits());
+				memset(this, 0, Length);
+				return _Bytes().PlusOne();
+			}
+			else {
+				_Bytes Quo(0), temp(that);
+				size_t diff = this->GetBits() - that.GetBits();
+				assert(this->GetBits() >= that.GetBits());
+				temp <<= diff;
+				for (size_t i = 0; i <= diff; i++)
+				{
+					if (*this >= temp) {
+						*this -= temp;
+						Quo.PlusOne();
+					}
+					temp >>= 1;
+					if (i != diff)
+					{
+						Quo <<= 1;
+					}
+				}
+				return Quo;
+			}
 		}
 		constexpr bool operator>(const _Bytes& that)const noexcept {
 			return (
