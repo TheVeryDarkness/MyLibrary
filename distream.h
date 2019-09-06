0c1828edf8fecdf368f3fbec18932b5091e02554
博奕 黄 <1853308@tongji.edu.cn>
2019-09-07 06:19:48 +0800

    Create distream.h

---
+++ distream.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "CustomizedBaseTraits.h"
+
+#ifdef _WIN32
+#include <conio.h>
+
+namespace LargeInteger {
+	enum source {
+		kb
+	};
+	template<source s,typename set>
+	class din {
+	public:
+		din() noexcept { }
+		~din() noexcept { }
+
+		template<typename Iter>
+		din& operator>>(Iter it) {
+			while (true) {
+				auto&& c = _getch();
+				switch (c) {
+				case '\n':
+					return *this;
+				default:
+					break;
+				}
+				auto&& i = set::to_int_type(c);
+				if (i!='?') {
+					it = i;
+					++it;
+				}
+			}
+		}
+	};
+}
+#endif // _WIN32
