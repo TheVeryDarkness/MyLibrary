98363fa9c77dd93a8a529f848e5a1eecea41db36
博奕 黄 <1853308@tongji.edu.cn>
2019-10-04 08:35:25 +0800

    Update VariableParameterTemplate.h

--- VariableParameterTemplate.h
+++ VariableParameterTemplate.h
@@ -1,39 +1,38 @@
 #pragma once
 
 namespace Template{
+	template<size_t, typename T, T, T...> constexpr static T get()noexcept;
+	template<typename func, typename T, T...>constexpr static T for_each();
 	template<typename T, T...>class Index;
 	template<size_t...> class product;
 
 
+	template<size_t index, typename T, T head, T... pack>
+	constexpr static T get()noexcept {
+		if constexpr (index == 0) {
+			return head;
+		}
+		else return get<index - 1, T, pack...>();
+	}
 
+
+	template<typename func, typename T, T...>constexpr static T for_each();
+	
 	template<typename T, T head, T... pack>
-	class Index<T, head, pack...> {
+	class Index<T, head, pack...> :public Index<T, pack...> {
 	public:
 		Index() = delete;
 		~Index() = delete;
-		constexpr static T get(T index)noexcept {
+		template<size_t index>
+		constexpr static T __stdcall get()noexcept {
+			static_assert(index <= sizeof...(pack), "The index is out of range.");
 			if constexpr (index == 0) {
 				return head;
 			}
-			else return Index<T, pack...>::get(index - 1);
+			else return Index<T, pack>::get<index - 1>();
 		}
 		constexpr static T getEnd()noexcept {
 			return Index<T, pack...>::getEnd();
-		}
-	private:
-	};
-
-	template<typename T, T head>
-	class Index<T, head> {
-	public:
-		Index() = delete;
-		~Index() = delete;
-		constexpr static T get(size_t index)noexcept {
-			static_assert(index == 0, "The index is out of range.");
-			return head;
-		}
-		constexpr static T getEnd()noexcept {
-			return head;
 		}
 	private:
 	};
