8e35e98229169cd122372ec681f2b0af58251a89
博奕 黄 <1853308@tongji.edu.cn>
2019-10-04 08:35:55 +0800

    Update VariableParameterTemplate.h

--- VariableParameterTemplate.h
+++ VariableParameterTemplate.h
@@ -3,7 +3,6 @@
 namespace Template{
 	template<size_t, typename T, T, T...> constexpr static T get()noexcept;
 	template<typename func, typename T, T...>constexpr static T for_each();
-	template<typename T, T...>class Index;
 	template<size_t...> class product;
 
 
@@ -17,25 +16,6 @@ namespace Template{
 
 
 	template<typename func, typename T, T...>constexpr static T for_each();
-	
-	template<typename T, T head, T... pack>
-	class Index<T, head, pack...> :public Index<T, pack...> {
-	public:
-		Index() = delete;
-		~Index() = delete;
-		template<size_t index>
-		constexpr static T __stdcall get()noexcept {
-			static_assert(index <= sizeof...(pack), "The index is out of range.");
-			if constexpr (index == 0) {
-				return head;
-			}
-			else return Index<T, pack>::get<index - 1>();
-		}
-		constexpr static T getEnd()noexcept {
-			return Index<T, pack...>::getEnd();
-		}
-	private:
-	};
 
 	template<size_t head, size_t ...pack>
 	class product<head, pack...> {
