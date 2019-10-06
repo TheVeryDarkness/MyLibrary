a02ce736a687d75fb9b2d81c7a216ce1fc900e49
博奕 黄 <1853308@tongji.edu.cn>
2019-10-06 22:03:41 +0800

    Update SignalVariable.h

--- SignalVariable.h
+++ SignalVariable.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <mutex>
+#include <atomic>
+#include <condition_variable>
 
 namespace Darkness {
 	class Empty {
@@ -41,10 +43,20 @@ namespace Darkness {
 			this->data = d;
 		}
 		template<typename Func> static auto both(Signal& a,Signal& b)noexcept {
-			std::unique_lock ula(a.locked_if_used);
-			std::unique_lock ulb(b.locked_if_used);
+			using namespace std::literals::chrono_literals;
+			while (true) {
+				bool _a = a.locked_if_used.try_lock();
+				bool _b = b.locked_if_used.try_lock();
+				if (_a && _b) break;
+				if (_a) a.locked_if_used.unlock();
+				if (_b) b.locked_if_used.unlock();
+				if (!_a && !_b) { std::this_thread::sleep_for(1ns); }
+				std::this_thread::sleep_for(1ns);
+			}
 			Func func;
 			auto&& res= func(a.data, b.data);
+			a.locked_if_used.unlock();
+			b.locked_if_used.unlock();
 			return res;
 		}
 		template<typename _Predicate = Empty>void wait(_Predicate && p = Empty())noexcept {
@@ -52,7 +64,7 @@ namespace Darkness {
 			changed_signal.wait(ul, p);
 		}
 	private:
-		T data;
+		std::atomic<T> data;
 		std::mutex locked_if_used;
 		std::condition_variable changed_signal;
 	};
