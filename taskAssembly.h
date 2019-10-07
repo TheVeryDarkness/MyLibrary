8eb7e6f5760dc13515216e8a7743bd8060879ff5
博奕 黄 <1853308@tongji.edu.cn>
2019-10-07 21:32:32 +0800

    Update taskAssembly.h

--- taskAssembly.h
+++ taskAssembly.h
@@ -19,7 +19,7 @@ namespace Darkness {
 		std::condition_variable wait_for_data[poolSize];
 		std::mutex locked_if_being_used;
 		std::condition_variable wait_for_thread;
-
+		std::chrono::duration dura;
 		bool available() const noexcept {
 			for (const auto &b : data_freshed) if (!b)	return true;
 			return false;
@@ -43,7 +43,7 @@ namespace Darkness {
 			return match(that);
 		}
 	public:
-		taskAssembly() = default;
+		taskAssembly(std::chrono::duration d = 1ns)noexcept :dura(d) { };
 		~taskAssembly()noexcept {
 			this->ending();
 			for (auto &w:wait_for_data) {
