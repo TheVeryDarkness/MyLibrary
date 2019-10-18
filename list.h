2b63a5f228a0c2b8b9afd8bf8476e792d9727440
博奕 黄 <1853308@tongji.edu.cn>
2019-10-18 22:43:53 +0800

    Update list.h

--- list.h
+++ list.h
@@ -1,32 +1,78 @@
 #pragma once
 
-#include "OLL.h"
-#include "DLL.h"
+#include "ConstantBuffer.h"
+#include "LinkedList.h"
 
 namespace LL {
-	template<typename Data, size_t align = 64, size_t CacheSize = 50>
-	class alignas(align) OAL :public OLL<Data[(align - sizeof(void *)) / sizeof(Data)], CacheSize> {
-		static_assert(align >= sizeof(void *) + sizeof(Data), "Alignment is too small.");
-		using super = OLL<Data[(align - sizeof(void *)) / sizeof(Data)], CacheSize>;
-	public:
-		OAL() { }
+	template<typename Data, size_t num, size_t CacheSize = 50>
+	class OAL {
+		MEMORY_CACHE(CacheSize);
+		class const_iterator {
+		public:
+			MY_LIB const_iterator(const OAL *_ptr):pA(_ptr),pD(_ptr->data) { }
+			MY_LIB ~const_iterator() { }
 
+			const Data& MY_LIB operator*()noexcept {
+				return *pD;
+			}
+			const_iterator &MY_LIB operator++()noexcept {
+				++pD;
+				if (pD - pA->data == num) {
+					pA = pA->next;
+					pD = pA->data;
+				}
+				return *this;
+			}
+			bool operator==(const const_iterator &that)const noexcept { 
+				return this->pA == that->pA && this->pD == that->pD;
+			}
+			bool operator!=(const const_iterator &that)const noexcept {
+				return !(*this == that);
+			}
+			bool operator==(nullptr_t)const noexcept { 
+				return this->pA == nullptr;
+			}
+			bool operator!=(nullptr_t)const noexcept {
+				return this->pA != nullptr;
+			}
+
+		private:
+			const OAL *pA;
+			const Data *pD;
+		};
+	protected:
+		Data data[num];
+		OAL *next;
+	public:
+		explicit OAL(Data head, OAL *next) :next(next) { data[0] = head; }
+		explicit OAL(Data head = Data()) :next(reinterpret_cast<OAL *>(this->data[1])) { data[0] = head; }
 		~OAL() { }
+		constexpr auto begin()const { return const_iterator(this); }
+		constexpr auto end()const { return nullptr; }
+		constexpr auto cbegin()const { return const_iterator(this); }
+		constexpr auto cend()const { return nullptr; }
+		void insert(Data n)noexcept {
+			assert(this != next);
+			if (
+				reinterpret_cast<size_t>(this) < reinterpret_cast<size_t>(next)
+				&&
+				reinterpret_cast<size_t>(next) < reinterpret_cast<size_t>(&next)
+				) {
+				*reinterpret_cast<Data *>(next) = n;
+				*reinterpret_cast<Data **>(&next) += 1;
+			}
+			else if (reinterpret_cast<size_t>(next) == reinterpret_cast<size_t>(&next)) {
+				this->next = new OAL(n);
+			}
+			else {
+				auto tmp = this->next;
+				this->next = new OAL(n, tmp);
+			}
+		}
+		void release()noexcept {
+			while (this->next!=nullptr) {
 
-	private:
-
+			}
+		}
 	};
-	template<typename Data, size_t align = 64, size_t CacheSize = 50>
-	class DAL :public DLL<Data[(align - 2 * sizeof(void *)) / sizeof(Data)], CacheSize> {
-		static_assert(align >= 2 * sizeof(void *) + sizeof(Data), "Alignment is too small.");
-		using super = DLL<Data[(align - 2 * sizeof(void *)) / sizeof(Data)], CacheSize>;
-	public:
-		DAL() { }
-
-		~DAL() { }
-
-	private:
-
-	};
-
 }
