a9fa302b7a44bbbb3c6e1bbb4110bf6487f023ea
博奕 黄 <1853308@tongji.edu.cn>
2019-10-19 19:53:25 +0800

    Update list.h

--- list.h
+++ list.h
@@ -28,6 +28,7 @@ namespace LL {
 			this->next = new OAL(n);
 		}
 		void next_move_forward()noexcept {
+			//assert(noMoreNode());
 			++ *reinterpret_cast<Data **>(&next);
 		}
 		void push_back_this(Data n)noexcept {
@@ -35,6 +36,11 @@ namespace LL {
 			*reinterpret_cast<Data *>(next) = n;
 			next_move_forward();
 		}
+	#ifdef _DEBUG
+	public:
+	#endif // _DEBUG
+		constexpr auto begin() { return iterator(this); }
+		constexpr auto end() { return nullptr; }
 	public:
 		using value_type = Data;
 		explicit OAL(Data head, OAL *next) :next(next) {
@@ -46,10 +52,8 @@ namespace LL {
 			data[0] = head; 
 		}
 		~OAL() { }
-		constexpr auto begin() { return iterator(this); }
 		constexpr auto begin()const { return const_iterator(this); }
 		constexpr auto cbegin()const { return const_iterator(this); }
-		constexpr auto end() { return nullptr; }
 		constexpr auto end()const { return nullptr; }
 		constexpr auto cend()const { return nullptr; }
 		constexpr bool noMoreNode()const noexcept {
@@ -107,7 +111,7 @@ namespace LL {
 				if (OprtPtr->hasVacancy()) {
 					memcpy(OprtPtr->data + 1, OprtPtr->data, sizeof(Data[num - 1]));
 					OprtPtr->data[0] = last;
-					next_move_forward();
+					OprtPtr->next_move_forward();
 				}
 				else if (OprtPtr->justFull()) {
 					OprtPtr->insert_node_after(last);
@@ -136,23 +140,26 @@ namespace LL {
 		constexpr static bool is_in(const OAL *pA, const Data *pD)noexcept {
 			return (pA->data <= pD) && (pD <= reinterpret_cast<const Data *>(&pA->next));
 		}
+		constexpr static bool legel_iterator_ptr(const OAL *pA, const Data *pD)noexcept {
+			return is_in(pA, pD) && (!is_in(pA, pA->next->data) || pD <= pA->next->data);
+		}
 		class iterator final{
 		public:
 			MY_LIB iterator(OAL *_ptr) :pA(_ptr), pD(_ptr->data) { }
 			MY_LIB ~iterator() { }
 
 			Data &MY_LIB operator*()noexcept {
-				assert(is_in(this->pA, this->pD));
-				return (*this == nullptr) ? ConstantBuffer<Data, 0>::get() : *pD;
+				assert(legel_iterator_ptr(pA, pD));
+				return (*this == nullptr) ? ConstantBuffer<Data, 0>::get() : *const_cast<Data *>(pD);
 			}
 			const Data &MY_LIB operator*()const noexcept {
-				assert(is_in(this->pA, this->pD));
+				assert(legel_iterator_ptr(pA, pD));
 				return (*this == nullptr) ? ConstantBuffer<Data, 0>::get() : *pD;
 			}
 			iterator &MY_LIB operator++()noexcept {
 				++pD;
 				if (reinterpret_cast<const Data *>(this->pA->next) == pD) {
-					pA->push_back(0);
+					const_cast<OAL *>(pA)->push_back(0);
 				}
 				if (pD == pA->data + num) {
 					pA = pA->next;
@@ -165,17 +172,18 @@ namespace LL {
 				for (size_t i = 0; i < sz; i++) {
 					if (it != nullptr) {
 						++(it.pD);
-						if (it.pD == it.pA->data + num) {
+						if (it.pD == it.pA->data + num && !pA->justFull()) {
 							assert(!it.pA->noMoreNode());
 							it.pA = it.pA->next;
 							it.pD = it.pA->data;
 						}
+						assert(is_in(it.pA, it.pD));
 					}
 				}
 				return it;
 			}
 			OAL *operator->() noexcept {
-				return this->pA;
+				return const_cast<OAL *>(this->pA);
 			}
 			const OAL *operator->()const noexcept {
 				return this->pA;
@@ -187,15 +195,16 @@ namespace LL {
 				return !(*this == that);
 			}
 			bool operator==(nullptr_t)const noexcept {
-				return reinterpret_cast<const Data *>(this->pA->next) == pD;
+				return this->pA->next->data == pD || reinterpret_cast<const Data *>(&this->pA->next) == pD;
+				//return reinterpret_cast<const Data *>(this->pA->next) == pD;
 			}
 			bool operator!=(nullptr_t)const noexcept {
 				return !(*this == nullptr);
 			}
 
 		private:
-			OAL *pA;
-			Data *pD;
+			const OAL *pA;
+			const Data *pD;
 		};
 
 
@@ -205,13 +214,13 @@ namespace LL {
 			MY_LIB ~const_iterator() { }
 
 			const Data &MY_LIB operator*()const noexcept {
-				assert(is_in(this->pA, this->pD));
+				assert(legel_iterator_ptr(pA, pD));
 				return (*this == nullptr) ? ConstantBuffer<Data, 0>::get() : *pD;
 			}
 			const_iterator &MY_LIB operator++()noexcept {
 				if (*this != nullptr) {
 					++pD;
-					if (pD == pA->data + num) {
+					if (pD == pA->data + num && !pA->justFull()) {
 						assert(!pA->noMoreNode());
 						pA = pA->next;
 						pD = pA->data;
@@ -237,7 +246,7 @@ namespace LL {
 				return !(*this == that);
 			}
 			bool operator==(nullptr_t)const noexcept {
-				return this->pA->next->data == pD;
+				return this->pA->next->data == pD || reinterpret_cast<const Data *>(&this->pA->next) == pD;
 			}
 			bool operator!=(nullptr_t)const noexcept {
 				return !(*this == nullptr);
