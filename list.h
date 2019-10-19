c6280e4b0c9cc80cd3f77a224e34878620468a4f
博奕 黄 <1853308@tongji.edu.cn>
2019-10-19 11:51:42 +0800

    Update list.h

--- list.h
+++ list.h
@@ -42,9 +42,11 @@ namespace LL {
 			data[0] = head; 
 		}
 		~OAL() { }
+		constexpr auto begin() { return iterator(this); }
 		constexpr auto begin()const { return const_iterator(this); }
-		constexpr auto end()const { return nullptr; }
 		constexpr auto cbegin()const { return const_iterator(this); }
+		constexpr auto end() { return nullptr; }
+		constexpr auto end()const { return nullptr; }
 		constexpr auto cend()const { return nullptr; }
 		constexpr bool noMoreNode()const noexcept {
 			return
@@ -76,12 +78,50 @@ namespace LL {
 			}
 		}
 	private:
+		class iterator final{
+		public:
+			MY_LIB iterator(OAL *_ptr) :pA(_ptr), pD(_ptr->data) { }
+			MY_LIB ~iterator() { }
+
+			Data &MY_LIB operator*()noexcept {
+				return *pD;
+			}
+			const Data &MY_LIB operator*()const noexcept {
+				return *pD;
+			}
+			iterator &MY_LIB operator++()noexcept {
+				++pD;
+				if (pD - pA->data == num) {
+					pA = pA->next;
+					pD = pA->data;
+				}
+				return *this;
+			}
+			bool operator==(const iterator &that)const noexcept {
+				return this->pA == that->pA && this->pD == that->pD;
+			}
+			bool operator!=(const iterator &that)const noexcept {
+				return !(*this == that);
+			}
+			bool operator==(nullptr_t)const noexcept {
+				return reinterpret_cast<const Data *>(this->pA->next) == pD;
+			}
+			bool operator!=(nullptr_t)const noexcept {
+				return !(*this == nullptr);
+			}
+
+		private:
+			OAL *pA;
+			Data *pD;
+		};
+
+
 		class const_iterator final{
 		public:
 			MY_LIB const_iterator(const OAL *_ptr) :pA(_ptr), pD(_ptr->data) { }
 			MY_LIB ~const_iterator() { }
 
-			const Data &MY_LIB operator*()noexcept {
+			const Data &MY_LIB operator*()const noexcept {
 				return *pD;
 			}
 			const_iterator &MY_LIB operator++()noexcept {
