c622fe229dfc1fd5bf47b287771a50513f0d1ed3
博奕 黄 <1853308@tongji.edu.cn>
2019-10-24 08:31:28 +0800

    Update MemoryCache.h
    堆栈式实现

--- MemoryCache.h
+++ MemoryCache.h
@@ -9,43 +9,34 @@ class MemoryCache
 public:
 	MY_LIB MemoryCache()noexcept{}
 
-	MY_LIB ~MemoryCache()noexcept{
-		for (auto& var : Cache)
-		{
-			if (var!=nullptr)
-			{
-				free(var);
-				var = nullptr;
-			}
+	MY_LIB ~MemoryCache()noexcept {
+		for (void **p = Cache + first_null - 1; Cache - 1 != p; --p) {
+			assert(*p != nullptr);
+			free(*p);
+			*p = nullptr;
 		}
+	#ifdef _DEBUG
+		for (auto i : Cache) {
+			assert(i == nullptr);
+		}
+	#endif // _DEBUG
 	}
 
 
 	void* pop(size_t Block) {
-	#ifndef _DEBUG
-		for (auto &val : Cache) {
-			if (val != nullptr) {
-				void *temp = val;
-				val = nullptr;
-				return temp;
-			}
-		}
-	#endif // !_DEBUG
-		return malloc(Block);
+		return (first_null == 0) ? malloc(Block) : Cache[--first_null];
 	}
 
-	void MY_LIB push(void* block)noexcept {
-	#ifndef _DEBUG
-		for (auto &val : Cache) {
-			if (val == nullptr) {
-				val = block;
-				return;
-			}
+	void MY_LIB push(void *block)noexcept {
+		if (first_null == CacheSize) {
+			return free(block);
 		}
-	#endif // !_DEBUG
-		return free(block);
+		else {
+			Cache[first_null++] = block;
+		}
 	}
 
 private:
 	void* Cache[CacheSize] = {};
+	size_t first_null = 0;
 };
