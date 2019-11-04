d32564f6342700a58cfa9e985f5234340a54a71a
博奕 黄 <1853308@tongji.edu.cn>
2019-11-04 09:18:16 +0800

    Update PreciseMath.h

--- PreciseMath.h
+++ PreciseMath.h
@@ -42,11 +42,7 @@ namespace LargeInteger {
 		}
 		template<typename val = double> val MY_LIB estim()const noexcept {
 			static_assert(std::is_arithmetic_v<val> && !std::is_integral_v<val>);
-			return (this->PosSign ?
-				(this->Numerator.GetValue<val>() / (this->Denominator.GetValue<val>()))
-				:
-				-(this->Numerator.GetValue<val>() / (this->Denominator.GetValue<val>()))
-				);
+			return (this->Numerator.GetValue<val>() / this->Denominator.GetValue<val>());
 		}
 		INLINED void MY_LIB destruct() noexcept {
 			this->Denominator.destruct();
