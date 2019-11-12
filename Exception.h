24860ed204cb9fe621cd075f2a094e592396ed72
博奕 黄 <1853308@tongji.edu.cn>
2019-11-12 22:05:25 +0800

    移除自定义异常

--- Exception.h
+++ Exception.h
@@ -2,27 +2,3 @@
 
 #include <stdexcept>
 #include <exception>
-
-
-namespace Darkness {
-	class RadixError :public std::exception {
-	public:
-		__stdcall RadixError(const char *);
-		__stdcall ~RadixError();
-		const char *what()const;
-	private:
-		const char *Message;
-	};
-	inline namespace Function {
-		class UnknownFunction :public std::exception {
-		public:
-			UnknownFunction(const char *msg, const std::string &name)
-				:std::exception(msg), functionName(name) { }
-			std::string what() {
-				return exception::what() + functionName;
-			}
-		private:
-			std::string functionName;
-		};
-	}
-}
