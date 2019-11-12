c18648ba98d7c6e7cb17859c4884c0812161ac89
博奕 黄 <1853308@tongji.edu.cn>
2019-11-12 22:03:41 +0800

    Update VisualStudioDebug.h
    降低对VS调试的依赖

--- VisualStudioDebug.h
+++ VisualStudioDebug.h
@@ -1,35 +1,14 @@
 #pragma once
 
-
-#ifdef _DEBUG
-#ifndef DEBUG_OUT
-
-#ifdef _WIN32
-
 #ifndef NOMINMAX
 #define NOMINMAX 1
 #endif // !NOMINMAX
-#include <Windows.h>
-
-#else
-#define DEBUG_OUT
-#define OutputDebugStringA(x)
-#define OutputDebugStringW(x)
-#define OutputDebugString(x)
-
-#endif // _WIN32
-
-#define DEBUG_OUT OutputDebugStringA((std::string(__FILE__) + " " + std::to_string(__LINE__) + "\n").c_str())
-
-#endif // !DEBUG_OUT
-#endif // _DEBUG
 
 #ifdef _DEBUG
 
 #ifndef NOMINMAX
 #define NOMINMAX 1
 #endif // NOMINMAX
-#include <crtdbg.h>
 #ifndef DBG_NEW
 #if 1
 #define DBG_NEW ::new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
@@ -42,11 +21,3 @@
 #define DBG_NEW new
 #endif // !DBG_NEW
 #endif  // _DEBUG
-
-
-#include <cassert>
-#ifdef _DEBUG
-#define MY_ASSERT(x,y) if(!(x))std::cerr<<(#y);assert(x);
-#else
-#define MY_ASSERT(x,y); 
-#endif
