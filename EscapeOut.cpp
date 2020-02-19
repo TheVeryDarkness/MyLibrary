690b1f22a97e1760ae501025cb16ba4f24f25d77
博奕 黄 <1853308@tongji.edu.cn>
2020-02-19 21:57:48 +0800

    更改功能

--- EscapeOut.cpp
+++ EscapeOut.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 
-constexpr inline char to_visible(char c)noexcept{
-	return ((32 <= c && c <= 126) || c & 0x80)? c: '.';
-}
 int main(int argc, char* argv[]) noexcept{
 	using namespace std;
 	if (argc <= 1) return cerr << "No file is provided in argument." << endl, -1;
@@ -15,7 +13,20 @@ int main(int argc, char* argv[]) noexcept{
 		}
 		cout << "/*Below is " << argv[i] << " */" << endl;
 		while (fin.peek(), fin){
-			cout.put(to_visible(fin.get()));
+			char c = fin.get();
+			switch(c){
+				case 7: cout << "\\a"; break;
+				case 8: cout << "\\b"; break;
+				case 9: cout << "\\t"; break;
+				case 10: cout << "\\n"; break;
+				case 11: cout << "\\v"; break;
+				case 12: cout << "\\f"; break;
+				case 13: cout << "\\r"; break;
+				default:
+					if ((0 <= c && c < 32) || c == 127)
+						cout << "\\x" << setw(3) << static_cast<unsigned>(c);
+					else cout << c;
+			}
 		}
 		cout << endl;
 		fin.close();
